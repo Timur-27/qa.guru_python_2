7f50d9a1b2d9267b3ad9e07337564f9a1cfa702b
Timur-27 <abdullin.timur27@gmail.com>
2022-06-17 18:50:17 +0300

    Add .gitignore file

--- test_google.py
+++ test_google.py
@@ -1,23 +1,18 @@
-from pytest import fixture
 from selene import have
 from selene.support.shared import browser
+import pytest
 
 
-
-
-
-@fixture()
+@pytest.fixture()
 def init_browser():
     browser.open('https://google.com').driver.set_window_size(160, 300)
 
 
 def test_search_selene(init_browser):
-
     browser.element('[name="q"]').type('selene user').press_enter()
     browser.element('[id = "search"]').should(have.text(' User-oriented Web'))
-
 
 
 def test_search_invalid_selene(init_browser):
     browser.element('[name="q"]').type('selene user').press_enter()
-    browser.element('[id = "search"]').should(have.text(' User-oriented Web'))
+    browser.element('[id = "search"]').should(have.text(' guru'))
